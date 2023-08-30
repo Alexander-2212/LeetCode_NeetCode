3bb91cc4849d600c6e13c44647e032b974a306b7
Alexander-2212 <kaminskiyale@gmail.com>
2023-08-30 18:16:03 +0300

    Problem 392. Is_Subsequence added

---
+++ 392_Is_Subsequence/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _392_Is_Subsequence
+{
+    public class Solution //TODO: Optimize result set
+    {
+        public bool IsSubsequence(string s, string t)
+        {
+            char[] sChars = s.ToCharArray();
+            char[] tChars = t.ToCharArray();
+
+            int counter = 0;
+
+            for (int i = 0; i < sChars.Length; i++)
+            {
+                if (tChars.Contains(sChars[i]))
+                {
+                    counter++;
+                }
+            }
+
+            return counter == sChars.Length;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
