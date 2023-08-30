f1a7e41d32ec9b8792cdb0f839386c2c835687e0
Alexander-2212 <kaminskiyale@gmail.com>
2023-08-30 18:00:36 +0300

    Problem 1929_Concatenation_of_Array Added

---
+++ 1929_Concatenation_of_Array/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1929_Concatenation_of_Array
+{
+    public class Solution
+    {
+        public int[] GetConcatenation(int[] nums)
+        {
+            int[] ans = new int[nums.Length * 2];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                ans[i] = nums[i];
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                ans[nums.Length + i] = nums[i];
+            }
+
+            return ans;
+        }
+    }
+
+    public class Solution2
+    {
+        public int[] GetConcatenation(int[] nums)
+        {
+            int[] ans = new int[nums.Length * 2];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                ans[i] = nums[i];
+                ans[nums.Length + i] = nums[i];
+            }
+
+            return ans;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
