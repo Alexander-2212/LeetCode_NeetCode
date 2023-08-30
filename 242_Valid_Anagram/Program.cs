8327350bcf1de9d3734cd01c08cad02c45d2902c
Alexander-2212 <kaminskiyale@gmail.com>
2023-08-30 17:54:15 +0300

    Problem 242_Valid_Anagram added

---
+++ 242_Valid_Anagram/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _242_Valid_Anagram
+{
+    public class Solution
+    {
+        public bool IsAnagram(string s, string t)
+        {
+            char[] sArr = s.ToCharArray();
+            char[] tArr = t.ToCharArray();
+
+            Array.Sort(sArr);
+            Array.Sort(tArr);
+
+            return sArr.SequenceEqual(tArr) ? true : false;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
