c33561bc4a42c0f455fca3bdd2a6ba93679878af
Alexander-2212 <kaminskiyale@gmail.com>
2023-08-30 18:17:55 +0300

    Problem 1. Two Sum added

---
+++ 1_Two_Sum/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Two_Sum
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int[] indices = new int[2];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        indices[0] = i;
+                        indices[1] = j;
+                    }
+                }
+            }
+
+            return indices;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
