496f1fa3688d4889bb5624250c2613bfa5dc1575
Alexander-2212 <kaminskiyale@gmail.com>
2023-08-30 20:33:38 +0300

    Problem 14. Longest common prefix solved

---
+++ 14_Longest_Common_Prefix/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_Longest_Common_Prefix
+{
+    static class Solution
+    {
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            //if (strs.Length == 0)
+            //    return "";
+
+            //string prefix = strs[0];
+
+            //for (int i = 1; i < strs.Length; i++)
+            //{
+            //    while (strs[i].IndexOf(prefix) != 0)
+            //    {
+            //        prefix = prefix.Substring(0, strs.Length - 1);
+            //        if (string.IsNullOrEmpty(prefix))
+            //            return "";
+            //    }
+
+            //}
+
+            //return prefix;
+
+            if (strs == null || strs.Length == 0)
+                return "";
+
+            Array.Sort(strs);
+
+            string first = strs[0];
+            string last = strs[strs.Length - 1];
+
+            int i = 0;
+
+            while (i < first.Length && i < last.Length && first[i] == last[i])
+            {
+                i++;
+            }
+
+            return first.Substring(0, i);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strs = { "flower", "flow", "flight" };
+            string[] strs2 = { "dog", "racecar", "car" };
+
+            Console.WriteLine(Solution.LongestCommonPrefix(strs));
+            Console.WriteLine(Solution.LongestCommonPrefix(strs2));
+
+            Console.ReadKey();
+        }
+    }
+}
